0d23ccd9fc3272303f78a57a624e5aff720a95df
Noor-26 <nk26982698@gmail.com>
2022-06-04 22:20:18 +0600

    connected with server and all projects get

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000
+const app = express()
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
+
+//middlewere
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.okboq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const handleProject = async () => {
+    try{
+            await client.connect()
+            const projectCollection = client.db("Portfolio").collection("projects");
+
+            app.get('/projects' ,async (req,res) => {
+                const getProjects = await projectCollection.find().toArray()
+                res.send(getProjects)
+            })
+
+    }finally{
+
+    }
+}
+handleProject()
+app.get('/',(req,res) => {
+    res.send('Success the surver is running')
+})
+
+app.listen(port,() => {
+    console.log('Connections to the port done'); 
+})
